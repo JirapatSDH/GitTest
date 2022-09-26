f6f56b40f7501d39ab15d126a256e43e9ef64d30
THANAPAT SUTHAMPAN <thanapat_sut@cmu.ac.th>
2022-09-26 09:35:52 +0700

    1.3

--- GitTest/Game1.cs
+++ GitTest/Game1.cs
@@ -10,6 +10,10 @@ namespace GitTest
         private SpriteBatch _spriteBatch;
         Texture2D ballTexture;
         Texture2D charTexture;
+        Vector2 charPosition = new Vector2(0, 250);
+        Vector2 ballPosition = new Vector2(250, 250);
+        bool personHit;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -34,21 +38,49 @@ namespace GitTest
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            GraphicsDevice device = _graphics.GraphicsDevice;
 
-            // TODO: Add your update logic here
+            KeyboardState keyboard = Keyboard.GetState();
 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            if (keyboard.IsKeyDown(Keys.Left))
+            {
+                charPosition.X = charPosition.X - 2;
+            }
+            if (keyboard.IsKeyDown(Keys.Right))
+            {
+                charPosition.X = charPosition.X + 2;
+            }
+            Rectangle charRectangle = new Rectangle((int)charPosition.X, (int)charPosition.Y, 32, 48);
+            Rectangle blockRectangle = new Rectangle((int)ballPosition.X, (int)ballPosition.Y, 24, 24);
+            if (charRectangle.Intersects(blockRectangle) == true)
+            {
+                personHit = true;
+            }
+            else if(charRectangle.Intersects(blockRectangle) == false)
+{
+                personHit = false;
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
+            GraphicsDevice device = _graphics.GraphicsDevice;
+            if (personHit == true)
+            { 
+                device.Clear(Color.Red); 
+            }
+            else
+            { 
+                device.Clear(Color.CornflowerBlue); 
+            }
+            
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(ballTexture, ballPosition, new Rectangle(24, 0, 24, 24), Color.White); 
+            _spriteBatch.Draw(charTexture, charPosition, new Rectangle(32, 0, 32, 48), Color.White);
+            _spriteBatch.End(); base.Draw(gameTime);
         }
     }
 }
